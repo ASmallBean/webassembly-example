0a7f819f6cb7465fc3040ec3da30b1655eb02c81
Claus <monkeydreamgo@gmail.com>
2021-10-20 17:50:19 +0800

    max

--- makefile
+++ makefile
@@ -1,3 +1,8 @@
-.PHONY: build
-build:
+.PHONY: build.wasm
+build.wasm:
 	@emcc src/main.cpp src/max.cpp -o ./dist/index.js
+
+
+.PHONY: build.cpp
+build.wasm:
+	@g++ src/main.cpp -o ./dist/run
--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main()
 
   std::cout << "Hello World!" << std::endl;
 
-	int max(int x, int y);//调用前声明一下max函数
+	int max_self(int x, int y);//调用前声明一下max函数
 	int a = 13, b = 130;
 	cout << "最大值为：" << max(a, b) << endl;
 	// system("pause");
