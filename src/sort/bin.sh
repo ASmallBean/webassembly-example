b93ce71b066136a32e463c004f6371207e49a3af
Claus <monkeydreamgo@gmail.com>
2021-10-25 08:29:17 +0000

    sort debug &7 wasi-app.c

--- src/sort/bin.sh
+++ src/sort/bin.sh
@@ -3,3 +3,9 @@ emcc index.cc --std=c++11 -s WASM=1 -o index.js --post-js post-script.js --pre-j
 emcc index.cc -O3 --std=c++11 -s WASM=1 -o index.js --post-js post-script.js --pre-js pre-script.js -s EXPORTED_RUNTIME_METHODS="['cwrap','getValue']"
 
 emcc index.cc -O3 --closure 1  --std=c++11 -s WASM=1 -o index.js --post-js post-script.js --pre-js pre-script.js -s EXPORTED_RUNTIME_METHODS="['cwrap','getValue']"
+
+EMCC_DEBUG=1 emcc index.cc -O3 --std=c++11 -s WASM=1 --no-entry -o index.js --post-js post-script.js --pre-js pre-script.js -s EXPORTED_RUNTIME_METHODS="['cwrap','getValue']"
+
+EMCC_DEBUG=2 emcc index.cc -O3 --std=c++11 -s WASM=1 --no-entry -o index.js --post-js post-script.js --pre-js pre-script.js -s EXPORTED_RUNTIME_METHODS="['cwrap','getValue']"
+
+EMCC_DEBUG=2 emcc index.cc -g4 -O3 --std=c++11 -s WASM=1 --no-entry -o index.js --post-js post-script.js --pre-js pre-script.js -s EXPORTED_RUNTIME_METHODS="['cwrap','getValue']"
