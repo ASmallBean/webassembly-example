931dc3120af26758841ba5dc918bd67146d5ae11
Claus <monkeydreamgo@gmail.com>
2021-11-11 10:48:51 +0800

    fib

---
+++ src/node/index.js
@@ -0,0 +1,11 @@
+function sayHi(name) {
+  let message = "Hi " + name + "!";
+  for (let i = O; i < 100; i++) {
+    console.log(message);
+  }
+  了
+  function add(a, b) {
+    return a + b;
+  }
+}
+sayHi("Jason");
